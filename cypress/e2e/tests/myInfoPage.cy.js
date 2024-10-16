06d7ec3a40b920ec1790b507c4a6277a17c0751e
dmeegodai <thanudilan@ymail.com>
2024-10-16 08:52:07 +0530

    Committed the myinfoPage name feild related test cases

--- cypress/e2e/tests/myInfoPage.cy.js
+++ cypress/e2e/tests/myInfoPage.cy.js
@@ -1,25 +1,37 @@
 import myinfoPage from "../../support/objectPages/myinfoPage";
 
+describe("Orange HRM POM Implementation for My Info page", () => {
+    beforeEach(() => {
+    cy.login("Admin", "admin123");
+  });
 
-describe("Orange HRM POM Implementation for My Info page", () =>{
-    before(()=>{
-        cy.login("Admin", "admin123");
+  describe("Navigate in to the My Info page", () => {
+    it("Should be able to click the My Info Text", () => {
+      myinfoPage.clickmyInfoText();
+      myinfoPage.validatemyInfoPageVisit();
     });
+  });
 
-    describe("Navigate in to the My Info page", () =>{
-        it("Should be able to click the My Info Text", () =>{
-            myinfoPage.clickmyInfoText();
-            myinfoPage.validatemyInfoPageVisit();
-        })
+  describe("Personal details tab postive senario", () => {
+    it("Should be able to fill complete name and submit", () => {
+      cy.navigateToMyInfoTab();
+      myinfoPage.enterUserFirstName("Thanura");
+      myinfoPage.enterUserMiddleName("Dilan");
+      myinfoPage.enterUserLastName("Meegoda");
+      myinfoPage.clickSaveButton();
+      myinfoPage.validateSuccsessMessage();
     });
+  });
 
-    describe("Personal details tab postive senario", () => {
-        it("Should be able to fill complete name and submit", ()=>{
-            cy.login("Admin", "admin123");
-            cy.navigateToMyInfoTab();
-            
-        })
-    })
-    
+  describe("Personal details tab negative senario", () => {
+    it("Should not be able to submit with incomplete name feilds", () => {
+      cy.navigateToMyInfoTab();
+      myinfoPage.enterUserFirstName(" ");
+      myinfoPage.enterUserMiddleName(" ");
+      myinfoPage.enterUserLastName(" ");
+      myinfoPage.clickSaveButton();
+      myinfoPage.validateWarningMessage();
+      //myinfoPage.validateSuccsessMessage();
+    });
+  });
 });
-
--- cypress/support/objectPages/myinfoPage.js
+++ cypress/support/objectPages/myinfoPage.js
@@ -5,20 +5,56 @@ class myinfoPage {
       cy.get(
         ".orangehrm-edit-employee-content > :nth-child(1) > .oxd-text--h6"
       ),
-    employeeFirstName: () => cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input'),
-    employeeMiddleName: cy.get(':nth-child(2) > :nth-child(2) > .oxd-input'),
-    employeeLastName:cy.get(':nth-child(3) > :nth-child(2) > .oxd-input'),
+    employeeFirstName: () =>
+      cy.get(
+        ".--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input"
+      ),
+    employeeMiddleName: () =>
+      cy.get(":nth-child(2) > :nth-child(2) > .oxd-input"),
+    employeeLastName: () =>
+      cy.get(":nth-child(3) > :nth-child(2) > .oxd-input"),
+    saveButton: () =>
+      cy.get(":nth-child(1) > .oxd-form > .oxd-form-actions > .oxd-button"),
+    firstNameWarning: () =>
+      cy.get(".--name-grouped-field > :nth-child(1) > .oxd-text"),
+    lastNameWarning: () =>
+      cy.get(".--name-grouped-field > :nth-child(3) > .oxd-text"),
   };
 
   // defined methods
   clickmyInfoText() {
     this.elements.myinfoText().click();
   }
+
   validatemyInfoPageVisit() {
-    this.elements.persoanlDetailTest ().should("have.text", "Personal Details");
+    this.elements.persoanlDetailTest().should("have.text", "Personal Details");
   }
+
   enterUserFirstName(fName) {
-    this.elements.employeeFirstName.clear().type(fname);
+    this.elements.employeeFirstName().clear().type(fName);
+  }
+
+  enterUserMiddleName(mName) {
+    this.elements.employeeMiddleName().clear().type(mName);
+  }
+
+  enterUserLastName(lName) {
+    this.elements.employeeLastName().clear().type(lName);
+  }
+
+  clickSaveButton() {
+    this.elements.saveButton().click();
+  }
+
+  validateSuccsessMessage() {
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal("Success");
+    });
+  }
+
+  validateWarningMessage() {
+    this.elements.firstNameWarning().should("have.text", "Required");
+    this.elements.lastNameWarning().should("have.text", "Required");
   }
 }
 
